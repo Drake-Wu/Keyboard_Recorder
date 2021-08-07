f1e3d9ff45f8b22c84c06d7a6ba0294159e51ac0
Drake Wu <244819714@qq.com>
2021-08-08 01:35:11 +0800

    添加停止按钮，和灰显效果

--- 键盘记录器/键盘记录器.cpp
+++ 键盘记录器/键盘记录器.cpp
@@ -5,14 +5,19 @@
 #include "键盘记录器.h"
 #include "iostream"
 #define MAX_LOADSTRING 100
+#define BUTTON_RECORD	0x0002
+#define BUTTON_REPEAT	0x0003
+#define RECORD_STOP	0x0004
+#define REPEAT_STOP	0x0005
+#define ID_TIMER_RECORD	0
+#define ID_TIMER_REPEAT	1
+
 #pragma warning(disable:4996)
 // 全局变量:
 HINSTANCE hInst;                                // 当前实例
 WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
 WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
-HANDLE hStdout;
-bool shift_status = false;
-short caps_status;
+static int keyboard_record_status = FALSE;
 // 此代码模块中包含的函数的前向声明:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
@@ -95,19 +100,46 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //        在此函数中，我们在全局变量中保存实例句柄并
 //        创建和显示主程序窗口。
 //
+HWND hBtnRecord = NULL;
+HWND hBtnRecordStop = NULL;
+HWND hBtnRepeat = NULL;
+HWND hBtnRepeatStop = NULL;
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 将实例句柄存储在全局变量中
 
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-   HWND hBtn = CreateWindow(
+   hBtnRecord = CreateWindow(
 	   TEXT("button"), //按钮控件的类名
 	   TEXT("键盘记录"),
 	   WS_CHILD | WS_VISIBLE | WS_BORDER | BS_FLAT/*扁平样式*/,
 	   30 /*X坐标*/, 20 /*Y坐标*/, 150 /*宽度*/, 50/*高度*/,
-	   hWnd, (HMENU)2 /*控件唯一标识符*/, hInst, NULL
+	   hWnd, (HMENU)BUTTON_RECORD /*控件唯一标识符*/, hInst, NULL
    );
+   hBtnRecordStop = CreateWindow(
+	   TEXT("button"), //按钮控件的类名
+	   TEXT("STOP"),
+	   WS_CHILD | WS_VISIBLE | WS_BORDER | BS_FLAT/*扁平样式*/,
+	   230 /*X坐标*/, 20 /*Y坐标*/, 150 /*宽度*/, 50/*高度*/,
+	   hWnd, (HMENU)RECORD_STOP /*控件唯一标识符*/, hInst, NULL
+   );
+   hBtnRepeat = CreateWindow(
+	   TEXT("button"), //按钮控件的类名
+	   TEXT("重复记录"),
+	   WS_CHILD | WS_VISIBLE | WS_BORDER | BS_FLAT/*扁平样式*/,
+	   30 /*X坐标*/, 100 /*Y坐标*/, 150 /*宽度*/, 50/*高度*/,
+	   hWnd, (HMENU)BUTTON_REPEAT /*控件唯一标识符*/, hInst, NULL
+   );
+   hBtnRepeatStop = CreateWindow(
+	   TEXT("button"), //按钮控件的类名
+	   TEXT("STOP"),
+	   WS_CHILD | WS_VISIBLE | WS_BORDER | BS_FLAT/*扁平样式*/,
+	   230 /*X坐标*/, 100 /*Y坐标*/, 150 /*宽度*/, 50/*高度*/,
+	   hWnd, (HMENU)REPEAT_STOP /*控件唯一标识符*/, hInst, NULL
+   );
+   EnableWindow(hBtnRecordStop, FALSE);
+   EnableWindow(hBtnRepeatStop, FALSE);
    if (!hWnd)
    {
       return FALSE;
@@ -140,36 +172,47 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_DESTROY  - 发送退出消息并返回
 //
 //
+
+void KeyBoardRecorder(HRAWINPUT hRawInput)
+{
+	RAWINPUT input = { 0 };
+	UINT size = sizeof(input);
+	GetRawInputData(hRawInput, RID_INPUT, &input, &size, sizeof(RAWINPUTHEADER));
+	if (input.data.keyboard.VKey <= '9' && input.data.keyboard.VKey >= '0' && input.data.keyboard.Flags == RI_KEY_MAKE)
+		printf("%c", input.data.keyboard.VKey);
+	else if (input.data.keyboard.VKey <= 'Z' && input.data.keyboard.VKey >= 'A' && input.data.keyboard.Flags == RI_KEY_MAKE)
+	{
+		if ((GetKeyState(VK_CAPITAL) & 0x1) ^ ((GetAsyncKeyState(VK_SHIFT) & 0x8000) >> 15))
+			printf("%c", input.data.keyboard.VKey);
+		else
+			printf("%c", input.data.keyboard.VKey + ('a' - 'A'));
+	}
+	else if (input.data.keyboard.VKey <= VK_NUMPAD9 && input.data.keyboard.VKey >= VK_NUMPAD0 && input.data.keyboard.Flags == RI_KEY_MAKE)
+		printf("%c", input.data.keyboard.VKey - (VK_NUMPAD0 - '0'));
+	return;
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
+	case WM_TIMER:
+		switch (wParam)
+		{
+		case ID_TIMER_RECORD:
+			KillTimer(hWnd, ID_TIMER_RECORD);
+			EnableWindow(hBtnRecordStop, TRUE);
+			keyboard_record_status = TRUE;
+			break;
+		case ID_TIMER_REPEAT:
+			break;
+		}
+		break;
 	case WM_INPUT:
 		{
 			HRAWINPUT hRawInput = (HRAWINPUT)lParam;
-			RAWINPUT input = { 0 };
-			UINT size = sizeof(input);
-			GetRawInputData(hRawInput, RID_INPUT, &input, &size, sizeof(RAWINPUTHEADER));
-			if (input.data.keyboard.VKey <= '9' && input.data.keyboard.VKey >= '0' && input.data.keyboard.Flags == RI_KEY_MAKE)
-				printf("%c", input.data.keyboard.VKey);
-			else if (input.data.keyboard.VKey <= 'Z' && input.data.keyboard.VKey >= 'A' && input.data.keyboard.Flags == RI_KEY_MAKE)
-			{
-				if((GetKeyState(VK_CAPITAL) & 0x1) ^ shift_status)
-					printf("%c", input.data.keyboard.VKey);
-				else
-					printf("%c", input.data.keyboard.VKey + ('a' - 'A'));
-			}
-			else if (input.data.keyboard.VKey <= VK_NUMPAD9 && input.data.keyboard.VKey >= VK_NUMPAD0 && input.data.keyboard.Flags == RI_KEY_MAKE)
-				printf("%c", input.data.keyboard.VKey - (VK_NUMPAD0 - '0'));
-			else if (input.data.keyboard.VKey == VK_LSHIFT ||
-				input.data.keyboard.VKey == VK_RSHIFT ||
-				input.data.keyboard.VKey == VK_SHIFT)
-			{
-				if (input.data.keyboard.Flags == RI_KEY_MAKE)
-					shift_status = true;
-				else if(input.data.keyboard.Flags == RI_KEY_BREAK)
-					shift_status = false;
-			}
+			if(keyboard_record_status)
+				KeyBoardRecorder(hRawInput);
 		}
 		break;
     case WM_COMMAND:
@@ -184,8 +227,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             case IDM_EXIT:
                 DestroyWindow(hWnd);
                 break;
-			case 2:
+			case BUTTON_RECORD:
+				SetTimer(hWnd, ID_TIMER_RECORD, 3000, 0);
+				EnableWindow(hBtnRecord, FALSE);
 				printf("keyboard\n");
+				break;
+			case RECORD_STOP:
+				EnableWindow(hBtnRecord, TRUE);
+				EnableWindow(hBtnRecordStop, FALSE);
+				keyboard_record_status = FALSE;
 				break;
             default:
                 return DefWindowProc(hWnd, message, wParam, lParam);
